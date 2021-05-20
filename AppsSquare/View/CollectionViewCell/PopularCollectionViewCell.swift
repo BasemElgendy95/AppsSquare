879c1e37cb4c9e2539275920d0d1cffd5af2965b
Basem Elgendy <basem_elgendy95@hotmail.com>
2021-05-20 22:27:59 +0200

    Create PopularCollectionViewCell

---
+++ AppsSquare/View/CollectionViewCell/PopularCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PopularCollectionViewCell.swift
+//  AppsSquare
+//
+//  Created by BasemElgendy on 20/05/2021.
+//
+
+import UIKit
+
+class PopularCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    @IBOutlet weak var favBtn: UIButton!
+
+}
