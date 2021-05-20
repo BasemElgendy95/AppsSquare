b8b9c993503ee19eaf07bc2f222216bb58dd9a77
Basem Elgendy <basem_elgendy95@hotmail.com>
2021-05-20 22:28:06 +0200

    Create PlaceImageCollectionViewCell

---
+++ AppsSquare/View/CollectionViewCell/PlaceImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceImageCollectionViewCell.swift
+//  AppsSquare
+//
+//  Created by BasemElgendy on 20/05/2021.
+//
+
+import UIKit
+
+class PlaceImageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    
+}
