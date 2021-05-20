66e38b91195e31a251916ba502019df6de6482df
Basem Elgendy <basem_elgendy95@hotmail.com>
2021-05-20 22:23:46 +0200

    Update UserModel

--- AppsSquare/Model/UserModel.swift
+++ AppsSquare/Model/UserModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+
+struct UserModel : Codable {
+    
+    let id : Int?
+    let fullName : String?
+    let email : String?
+    let phone : String?
+    let status : Int?
+    let role : String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case fullName = "fullName"
+        case email = "email"
+        case phone = "phone"
+        case status = "status"
+        case role = "role"
+    }
+           
+}
