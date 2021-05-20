d14572a9d355a0c2fb8642d52737e1c2fe03dd3d
Basem Elgendy <basem_elgendy95@hotmail.com>
2021-05-20 22:22:32 +0200

    Create CategoryModel

---
+++ AppsSquare/Model/CategoryModel.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryModel.swift
+//  AppsSquare
+//
+//  Created by BasemElgendy on 20/05/2021.
+//
+
+import UIKit
+
+struct CategoryModel  {
+
+    let title : String?
+    let image : UIImage?
+    let color : UIColor?
+    
+}
