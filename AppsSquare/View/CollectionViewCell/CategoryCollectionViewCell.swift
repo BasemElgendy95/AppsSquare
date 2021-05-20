7f807c28bdbcc4c26249cffc8e918beefbe0da1d
Basem Elgendy <basem_elgendy95@hotmail.com>
2021-05-20 22:28:19 +0200

    Create CategoryCollectionViewCell

---
+++ AppsSquare/View/CollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryCollectionViewCell.swift
+//  AppsSquare
+//
+//  Created by BasemElgendy on 20/05/2021.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var bgView: UIView!
+}
