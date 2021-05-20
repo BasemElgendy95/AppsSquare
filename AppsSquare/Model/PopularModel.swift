479ca900642ebf916b107e4f87540cc38de6d92a
Basem Elgendy <basem_elgendy95@hotmail.com>
2021-05-20 22:22:44 +0200

    Create PopularModel

---
+++ AppsSquare/Model/PopularModel.swift
@@ -0,0 +1,17 @@
+//
+//  PopularModel.swift
+//  AppsSquare
+//
+//  Created by BasemElgendy on 20/05/2021.
+//
+
+import UIKit
+
+struct PopularModel  {
+
+    let title : String?
+    let subTitle : String?
+    let image : String?
+    var isFav : Bool?
+    
+}
